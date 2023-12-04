79b432d35712a2736dff419bafe23f235d935efe
Jabloon <alperen.tasdelen@metu.edu.tr>
2023-12-04 15:00:12 +0300

    main changed to rasterizer

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
+#include "Scene.h"
 
 using namespace std;
 
-//TODO: Start implementations to the Main
+//TODO: Start implementations here
 /*
     1. Transform vertices to the viewport
     2. Line drawing algorithm
@@ -24,3 +26,39 @@ using namespace std;
     - Depth Buffer Algorithm
     - A makefile should be provided (make rasterizer)
 */
+
+Scene *scene;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cout << "Please run the rasterizer as:" << endl
+             << "\t./rasterizer <input_file_name>" << endl;
+        return 1;
+    }
+    else
+    {
+        const char *xmlPath = argv[1];
+
+        scene = new Scene(xmlPath);
+
+        for (int i = 0; i < scene->cameras.size(); i++)
+        {
+            // initialize image with basic values
+            scene->initializeImage(scene->cameras[i]);
+
+            // do forward rendering pipeline operations
+            scene->forwardRenderingPipeline(scene->cameras[i]);
+
+            // generate PPM file
+            scene->writeImageToPPMFile(scene->cameras[i]);
+
+            // Converts PPM image in given path to PNG file, by calling ImageMagick's 'convert' command.
+            // Change/remove implementation if necessary.
+            scene->convertPPMToPNG(scene->cameras[i]->outputFilename);
+        }
+
+        return 0;
+    }
+}
